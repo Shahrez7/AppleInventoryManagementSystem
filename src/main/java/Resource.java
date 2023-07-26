5358254ca53470bafda02bb5cece2c72f0c16d71
Shahrez7 <116435332+Shahrez7@users.noreply.github.com>
2023-07-26 13:18:49 +0500

    Logging Done

--- src/main/java/Resource.java
+++ src/main/java/Resource.java
@@ -1,12 +1,9 @@
 import com.google.gson.Gson;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
 
 @Path("/path")
 public class Resource {
@@ -55,9 +52,12 @@ public class Resource {
     @Produces(MediaType.APPLICATION_JSON)
     public String FetchAllByCategory(@PathParam("categoryId") int categoryId) {
         try {
+            LOGGER.info("GET Request Received: Fetch AlL by Category: "+categoryId);
             return new Gson().toJson(App.FetchAllByCategory(categoryId));
         } catch (Exception exc) {
+            LOGGER.error("Error Occurred!");
             exc.printStackTrace();
+
         }
         return null;
     }
@@ -66,8 +66,10 @@ public class Resource {
     @Produces(MediaType.APPLICATION_JSON)
     public String FetchAllByLocation(@PathParam("locationId") int locationId) {
         try {
+            LOGGER.info("GET Request Received: Fetch AlL by Location: "+locationId);
             return new Gson().toJson(App.FetchAllByLocation(locationId));
         } catch (Exception exc) {
+            LOGGER.error("Error Occurred!");
             exc.printStackTrace();
         }
         return null;
@@ -77,9 +79,11 @@ public class Resource {
     @Produces(MediaType.APPLICATION_JSON)
     public String FetchAllByLocationAndCategory(@PathParam("locationId") int locationId, @PathParam("categoryId") int categoryId) {
         try {
+            LOGGER.info("GET Request Received: Fetch AlL by Location: "+locationId+ " and Category: "+categoryId);
             return new Gson().toJson(App.FetchAllByLocationAndCategory(locationId, categoryId));
         } catch (Exception exc) {
             exc.printStackTrace();
+            LOGGER.error("Error Occurred!");
         }
         return null;
     }
@@ -90,11 +94,14 @@ public class Resource {
         try {
             boolean isDeleted = App.deleteInventoryItemById(InvId);
             if (isDeleted) {
+                LOGGER.info("Delete Request Received: Delete Inventory Item ID : "+InvId);
                 return Response.status(Response.Status.OK).entity("{\"message\":\"Inventory item deleted successfully.\"}").build();
             } else {
+                LOGGER.error("Error: Item ID Not Found");
                 return Response.status(Response.Status.NOT_FOUND).entity("{\"error\":\"Inventory item not found.\"}").build();
             }
         } catch (Exception exc) {
+            LOGGER.error("Error Occurred!");
             exc.printStackTrace();
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("{\"error\":\"An error occurred.\"}").build();
         }
@@ -110,13 +117,15 @@ public class Resource {
         Inventory addedItem = App.addInventoryItem(newItem);
 
         if (addedItem != null) {
-
+            LOGGER.info("Post Request Received: Item Added");
             String responseJson = gson.toJson(addedItem);
             return Response.status(Response.Status.OK).entity(responseJson).build();
         } else {
+            LOGGER.error("Error Occurred!");
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("{\"error\":\"Failed to add inventory item.\"}").build();
         }
     }
+
     @PUT
     @Path("/{inventory_id}")
     @Consumes(MediaType.APPLICATION_JSON)
@@ -127,9 +136,11 @@ public class Resource {
         updatedItem.setId(inventoryId);
         Inventory result = App.updateInventoryItem(updatedItem);
         if (result != null) {
+            LOGGER.info("Put Request Received: Update Inventory Item ID : "+inventoryId);
             String responseJson = gson.toJson(result);
             return Response.status(Response.Status.OK).entity(responseJson).build();
         } else {
+            LOGGER.error("Error Occurred!");
             return Response.status(Response.Status.NOT_FOUND).entity("{\"error\":\"Inventory item not found.\"}").build();
         }
     }
